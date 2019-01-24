e52835c1ca68ed31158f9d0d7bc5c18be3b90c0c
Beatriz Parejo Ramos <32536069+BeatrizPR@users.noreply.github.com>
2019-01-24 13:39:09 +0100

    continue the module

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormBuilder} from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -13,15 +13,17 @@ export class CustomerComponent implements OnInit {
  
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
-    });
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      // if i want a value input disable, i can do that
+      //lastName: {value: 'n/a', disabled: true},
+      email: '',
+      sendCatalog:true
+    })
   }
 
   populateTestData(): void{
