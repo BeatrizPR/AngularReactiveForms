0065f18f8f5622eeca9faf967bbc0f2b24525c45
Beatriz Parejo Ramos <32536069+BeatrizPR@users.noreply.github.com>
2019-01-25 13:37:06 +0100

    continue the module

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray} from '@angular/forms';
 
 import { Customer } from './customer';
+import { Subscriber } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 // check if the email and the confirmEmail is the same
 function emailMatcher(c: AbstractControl): {[key: string]: boolean} | null {
@@ -39,10 +41,21 @@ export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
  
   customer = new Customer();
+  emailMessage: string;
+
+  get addresses(): FormArray{
+    return <FormArray>this.customerForm.get('addresses');
+  }
+
+  private validationMessages ={
+    required: 'Please enter you email address.',
+    email: 'Please enter a valid email address.'
+  };
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
     this.customerForm = this.fb.group({
       // cwith validators required y minLength we can delete that validations in the html page
       firstName: ['', [Validators.required, Validators.minLength(3)]],
@@ -55,10 +68,35 @@ export class CustomerComponent implements OnInit {
       phone: '',
       notification: 'email',
       sendCatalog:true,
-      rating : [null, ratingRange(1,5)]
+      rating : [null, ratingRange(1,5)],
 
+      addresses: this.fb.array([this.buildAddress() ]),
+      
       // if i want a value input disable, i can do that
       //lastName: {value: 'n/a', disabled: true}
+    });
+
+    //                                                    ^
+    // this formGroup must be after the root FormGroup -  |
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    );
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.pipe(debounceTime(1000)).subscribe(
+      value => this.setMessage(emailControl)
+    );
+
+  }
+
+  buildAddress(): FormGroup{
+    return this.fb.group({
+      addressType: 'home',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: '',
     })
   }
 
@@ -73,6 +111,14 @@ export class CustomerComponent implements OnInit {
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setMessage(c: AbstractControl): void{
+    this.emailMessage = '';
+    if((c.touched || c.dirty) && c.errors){
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.emailMessage += this.validationMessages[key]). join(' ');
+    }
   }
 
   setNotification(notifyVia: string): void{
